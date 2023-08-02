3458f635c86065e8e2078ad652b41c55649e8ebe
Ahadzhan Piloian <ahadzhan.piloian@gmail.com>
2023-08-02 13:50:22 +0400

    added parsing struct

---
+++ src/builtin/builtin.c
@@ -0,0 +1,60 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   builtin.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: apiloian <apiloian@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/12 13:56:32 by apiloian          #+#    #+#             */
+/*   Updated: 2023/08/01 21:01:42 by apiloian         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../includes/minishell.h"
+
+void	pwd(void)
+{
+	char	dir[PATH_MAX];
+
+	getcwd(dir, PATH_MAX); 
+	printf("%s\n", dir);
+}
+
+// void	cd(char **args)
+// {
+// 	// if (!args[1])
+// 	while (*args[1] == '.' && *(args[1] + 1) == '.')
+// 	{
+// 		chdir("..");
+// 		args[1]++;
+// 	}
+// 		chdir(args[1]);
+// }
+
+void	echo(char **args)
+{
+	int	i;
+	int	flag;
+
+	i = 1;
+	flag = 0;
+	while (args[i] && ft_strncmp(args[i], "-n", 2) == 0)
+	{
+		flag = 1;
+		i++;
+	}
+	while (args[i])
+	{
+		printf("%s", args[i]);
+		i++;
+		if (args[i])
+			printf(" ");
+	}
+	if (!flag)
+		printf("\n");
+}
+
+// void	env(t_data *data)
+// {
+	
+// }
