23c4e82f95be0d62acbc3703b19d05277260ed37
Hikabu <bledniyadozub@gmail.com>
2023-09-17 18:51:02 +0400

    export env works

--- src/utils/builtin_utils.c
+++ src/utils/builtin_utils.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   builtin_utils.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: timelkon <timelkon@student.42.fr>          +#+  +:+       +#+        */
+/*   By: vfedorov <vfedorov@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/04 18:24:02 by apiloian          #+#    #+#             */
-/*   Updated: 2023/09/16 20:52:58 by timelkon         ###   ########.fr       */
+/*   Updated: 2023/09/17 18:28:33 by vfedorov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -56,7 +56,7 @@ int	check_builtin(t_parse *cmd, t_data *data)
 			close(cmd->fd_in), close(cmd->fd_out), 1);
 	else if (ft_strncmp(cmd->cmd[0], "env", 3) == 0
 		&& ft_strncmp(cmd->cmd[0], "env", ft_strlen(cmd->cmd[0])) == 0)
-		return (env(data), close(cmd->fd_in), close(cmd->fd_out), 1);
+		return (env(data, cmd), close(cmd->fd_in), close(cmd->fd_out), 1);
 	else if (ft_strncmp(cmd->cmd[0], "export", 6) == 0
 		&& ft_strncmp(cmd->cmd[0], "export", ft_strlen(cmd->cmd[0])) == 0)
 		return (export(data, cmd), close(cmd->fd_in), close(cmd->fd_out), 1);
--- src/utils/print_utils.c
+++ src/utils/print_utils.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   print_utils.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: timelkon <timelkon@student.42.fr>          +#+  +:+       +#+        */
+/*   By: vfedorov <vfedorov@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/31 17:14:34 by apiloian          #+#    #+#             */
-/*   Updated: 2023/09/16 21:44:42 by timelkon         ###   ########.fr       */
+/*   Updated: 2023/09/17 18:42:26 by vfedorov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,9 @@ void	print_linked_list(t_env *head)
 	current = head;
 	while (current != NULL)
 	{
-		if (*current->key && *current->val)
+		if (!*current->key && !*current->val)
+			printf("%s", current->key);
+		else if (*current->key && *current->val)
 			printf("%s=%s\n", current->key, current->val);
 		else if (current->flag == 1)
 			printf("%s=\n", current->key);
